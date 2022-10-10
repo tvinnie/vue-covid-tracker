f74d08264eea1a14de1e38d4bcb4e37426d8e45f
tvinnie <tvinnie77@gmail.com>
2022-10-10 22:56:54 +0300

    COVID-19 Tracker from an API

---
+++ src/components/DataBoxes.vue
@@ -0,0 +1,52 @@
+<template>
+   <div class="grid md:grid-cols-2 gap-4">
+
+    <!-- Box layout -->
+    <div class="shadow-md bg-blue-100 p-10 text-center rounded">
+        <h3 class="text-3xl text-blue-900 font-bold mb-4">Cases</h3>
+
+        <div class="text-2xl mb-4">
+            <span class="font-bold">New:</span>
+            {{numberWithComma( stats.NewConfirmed )}}
+        </div>
+        <div class="text-2xl mb-4">
+            <span class="font-bold">Total:</span>
+            {{numberWithComma( stats.TotalConfirmed ) }}
+
+        </div>
+    </div>
+
+
+
+    <!-- Box layout -->
+    <div class="shadow-md bg-blue-200 p-10 text-center rounded">
+        <h3 class="text-3xl text-blue-900 font-bold mb-4">Deaths</h3>
+
+        <div class="text-2xl mb-4">
+            <span class="font-bold">New:</span>
+            {{numberWithComma( stats.NewDeaths)}}
+        </div>
+        <div class="text-2xl mb-4">
+            <span class="font-bold">Total:</span>
+            {{numberWithComma( stats.TotalDeaths)}}
+
+        </div>
+    </div>
+
+
+   </div>
+</template>
+ <script>
+
+ export default {
+    name: 'DataBoxes',
+    props: ['stats'],
+    methods: {
+        numberWithComma(x) {
+            return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g,',')
+        }
+    }
+ }
+ </script>
+
+ 
